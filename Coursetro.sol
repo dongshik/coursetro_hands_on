add8a22ab19c9895bdd53e3f47ab458929c14a33
dongsik <dongshik.lee@gmail.com>
2017-12-16 23:11:05 +0900

    class 1 complete

--- Coursetro.sol
+++ Coursetro.sol
@@ -1,6 +1,15 @@
-//defines the version of solidity you're going to use
 pragma solidity ^0.4.18;
 
 contract Coursetro {
+    string fName;
+    uint age;
+
+    function setInstructor(string _fName, uint _age) public {
+        fName = _fName;
+        age = _age;
+    }
     
+    function getInstructor() public constant returns(string, uint) {
+        return(fName, age);
+    }
 }
